c4f526329df2ba55400fe954cedecd4595f492d0
LAGonauta <lagonauta@gmail.com>
2021-03-26 08:10:58 -0300

    Remove unity compilation unit

--- plugins/windows-notification/api/src/unity.cpp
+++
@@ -1,5 +0,0 @@
-#include "./gobject/winrt.cpp"
-#include "./gobject/winrt-toast-notifier.cpp"
-#include "./gobject/winrt-toast-notification.cpp"
-#include "./gobject/winrt-event-token.cpp"
-#include "./gobject/winrt-enums.cpp"
