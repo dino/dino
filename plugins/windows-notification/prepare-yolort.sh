c57fc768aa19937866f823a1ad2b70e62fc2b870
LAGonauta <lagonauta@gmail.com>
2021-06-19 11:43:12 -0300

    Do not generate WinRT headers, just download them on build

--- plugins/windows-notification/prepare-yolort.sh
+++ plugins/windows-notification/prepare-yolort.sh
@@ -1,15 +1,10 @@
 #!/bin/bash
-
+export FILE_NAME=cppwinrt-2.0.210122.3+windows-10.0.19041+yolort-835cd4e.zip
 export ORIGINAL_FOLDER=${PWD}
-cd ${PWD}/yolort-builder
 
-if [ ! -f bin/cppwinrt.exe ]; then
-    make CPPWINRT_VERSION=2.0.210122.3 bin/cppwinrt.exe
-fi
-
-make originals
-make --jobs=$(nproc)
-
-mkdir -p ../yolort
-cp -r include ../yolort
-cd ${ORIGINAL_FOLDER}
+mkdir -p yolort && \
+    cd yolort && \
+    curl -L -O https://github.com/LAGonauta/YoloRT/releases/download/v1.0.0/${FILE_NAME} && \
+    unzip -o ${FILE_NAME} && \
+    rm ${FILE_NAME} && \
+    cd ${ORIGINAL_FOLDER}
