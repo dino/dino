97753bd5c411908624b8016bdd83f6863cfca09a
LAGonauta <lagonauta@gmail.com>
2021-06-19 06:58:40 -0300

    Remove YoloRT from tree

--- plugins/windows-notification/yolort/include/winrt/Windows.Data.Xml.Dom.h
+++
@@ -1,3 +0,0 @@
-#define HEADER_IMPL <winrt/yolort_impl/winrt/Windows.Data.Xml.Dom.h>
-#include <winrt/yolort_impl/yolo.ipp>
-#undef HEADER_IMPL
--- plugins/windows-notification/yolort/include/winrt/Windows.UI.Notifications.h
+++
@@ -1,3 +0,0 @@
-#define HEADER_IMPL <winrt/yolort_impl/winrt/Windows.UI.Notifications.h>
-#include <winrt/yolort_impl/yolo.ipp>
-#undef HEADER_IMPL
